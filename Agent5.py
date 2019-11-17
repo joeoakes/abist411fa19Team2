027e382fca818e9fd21f661e4fa05afcdffca3d1
Iraina Lambe <36733602+ial5049@users.noreply.github.com>
2019-11-17 14:31:26 -0500

    Agent5

---
+++ Agent5.py
@@ -0,0 +1,16 @@
+import sys, json, urllib.request, ssl, socket
+from pymongo import MongoClient
+
+# Project App 5
+# Purpose: mongo5 - logger for project diamond
+
+try:
+ #saving workflow action
+ client = MongoClient('localhost', 27017)
+ db = client.Team2
+ collection = db.agent1
+ print("Saved workflow action")
+ post_id = collection.insert({"action" : "Payload Sent"})
+
+except Exception as e:
+ print(e)
